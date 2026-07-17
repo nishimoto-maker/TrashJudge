1c630cec9a700f5986eeeb608ad5a6e9bb6dee5a
nishimoto-maker <nishinish6121@gmail.com>
2026-07-17 10:18:40 +0900

    a

--- apps/judge/forms.py
+++ apps/judge/forms.py
@@ -3,19 +3,17 @@ from flask_wtf.form import FlaskForm
 from wtforms.fields.simple import SubmitField
 
 
-# 画像アップロード用のフォームのもとを作成
 class UploadImageForm(FlaskForm):
-    # ファイルフィールドに必要なバリデーションを設定する
-    image = FileField( # ファイルのアップロード用のフィールドを作成する
+    image = FileField(
         validators=[
-            FileRequired("画像ファイルを指定してください。"), # アップロード必須
-            FileAllowed(["png", "jpg", "jpeg", "gif"], "サポートされていない画像形式です。"), # アップロードできる画像の拡張子を指定
+            FileRequired("画像ファイルを指定してください。"),
+            FileAllowed(["png", "jpg", "jpeg", "gif"], "サポートされていない画像形式です。"),
         ]
     )
-    submit = SubmitField("アップロード") # アップロードボタンを作成する
+    submit = SubmitField("アップロード")
 
-class JudgeForm(FlaskForm): # 物体検知用のフォームのもとを作成する
-    submit = SubmitField("検知") # 物体検知の実行ボタンを作成する
+class JudgeForm(FlaskForm):
+    submit = SubmitField("検知")
 
-class DeleteForm(FlaskForm): # 画像削除用のフォームのもとを作成する
-    submit = SubmitField("削除") # 画像削除の実行ボタンを作成する
+class DeleteForm(FlaskForm):
+    submit = SubmitField("削除")
